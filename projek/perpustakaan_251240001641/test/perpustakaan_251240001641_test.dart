fadc494f9725f8cabb78c5e81b8f0fc9141f57a3
Schyter <159677573+NabilAriq@users.noreply.github.com>
2026-04-13 22:13:42 +0700

    Delete projek directory

--- projek/perpustakaan_251240001641/test/perpustakaan_251240001641_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:perpustakaan_251240001641/perpustakaan_251240001641.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
